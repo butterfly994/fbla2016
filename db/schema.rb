72a252c18b9d77d7fc5439eae91abd543289b164
adabelle <adabelle.x@gmail.com>
2016-03-03 21:07:52 -0500

    have to create the owner user

--- db/schema.rb
+++ db/schema.rb
@@ -84,7 +84,4 @@ ActiveRecord::Schema.define(version: 20160301022837) do
     t.string   "name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
 end
